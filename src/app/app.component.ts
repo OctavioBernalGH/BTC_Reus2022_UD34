b1a8e7fbb7702e2f4f08e26e68043c44e78c8f6b
OctavioBernalGH <103035621+OctavioBernalGH@users.noreply.github.com>
2022-06-02 13:19:34 +0200

    svcddfasc
    cdacsd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { NumberFormatStyle } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Primer-proyecto-angular';
+  title = 'HTML-CSS-ANGULAR-TYPESCRIPT';
+  name = 'Octavio';
+
+
 }
